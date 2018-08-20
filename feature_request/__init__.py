e5beacef847c6930ce685ca146276273aad9fc63
Mohamed Salah <googexel@gmail.com>
2018-08-20 17:52:32 +0200

    add data format to config, remove track modification

--- feature_request/__init__.py
+++ feature_request/__init__.py
@@ -3,30 +3,14 @@ import os
 from flask import Flask
 
 
-class InvalidUsage(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        rv['status'] = self.status_code
-        return rv
-
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev-key-123',
         SQLALCHEMY_DATABASE_URI=os.path.join('sqlite:////' + app.instance_path, 'req_mg.db'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=True
+        SQLALCHEMY_TRACK_MODIFICATIONS=True,
+        DATE_FORMAT='%Y-%m-%d',
     )
 
     if test_config is None:
@@ -42,12 +26,16 @@ def create_app(test_config=None):
     except OSError as Eos:
         app.logger.info('Failed to create instance folder: %s' % Eos)
 
-    from . import db
+    from feature_request import db
     db.init_app(app)
 
-    from . import req_mg_api, views
+    from feature_request import views
+    from feature_request import req_mg_api
+
     # add request page
     app.register_blueprint(req_mg_api.bp)
+
+    # could be removed if we need it API only
     app.register_blueprint(views.request_bp)
     app.add_url_rule('/', endpoint='index')
 
